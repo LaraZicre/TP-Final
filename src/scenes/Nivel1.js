7f7854c8b88792b50fa3e77fbdebeeb79de4eab2
LaraZicre <l.zicre@gmail.com>
2023-06-21 02:41:42 -0300

    add spritesheet animation

--- src/scenes/Nivel1.js
+++ src/scenes/Nivel1.js
@@ -31,15 +31,42 @@ export default class Nivel1 extends Phaser.Scene {
       console.log(spawntPoint);
       //add sprite to the player
       this.oso = this.physics.add.sprite(spawntPoint.x, spawntPoint.y, "oso");
+
+      //  Player physics properties. Give the little guy a slight bounce.
+      this.oso.setBounce(0.1);
+      this.oso.setCollideWorldBounds(true);
       
       //add colision for tile and platforms with the player
       capatiles.setCollisionByProperty({ colision: true });
       capaplataformas.setCollisionByProperty({ colision: true });
       this.physics.add.collider(this.oso, capatiles);
       this.physics.add.collider(this.oso, capaplataformas);
+
+      this.cursors = this.input.keyboard.createCursorKeys();
+
     }
 
     update() {
+
+      if (this.cursors.left.isDown) {
+        this.oso.setVelocityX(-160);
+        this.oso.anims.play("left", true);
+      }
+      //move right
+      else if (this.cursors.right.isDown) {
+        this.oso.setVelocityX(160);
+        this.oso.anims.play("right", true);
+      }
+      //stop
+      else {
+        this.oso.setVelocityX(0);
+        this.oso.anims.play("turn");
+      }
+  
+      //jump
+      if (this.cursors.up.isDown && this.oso.body.blocked.down) {
+        this.oso.setVelocityY(-330);
+      }
   
     }
   }
--- src/scenes/Preload.js
+++ src/scenes/Preload.js
@@ -11,7 +11,7 @@ export default class Preload extends Phaser.Scene {
       // load assets
       this.load.tilemapTiledJSON("level1", "public/assets/tilemaps/map1.json");
       this.load.image("tiles", "public/assets/images/Tileset.png");
-      this.load.spritesheet("oso", "./public/assets/images/SpriteSheet/oso.png", {
+      this.load.spritesheet("oso", "./public/assets/images/SpriteSheet/oso 40x60.png", {
         frameWidth: 40,
         frameHeight: 60,
       });
